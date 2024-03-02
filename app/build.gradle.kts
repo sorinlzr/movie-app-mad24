0baa381d1673fb76d37d20e6f3173c6206a4a6bd
sorin.lzr <soso.laz@gmail.com>
2024-03-02 18:15:16 +0100

    EX2: update dependencies and permissions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
