6aa15a7172fc2da2b295db57037dae2d1526add2
sorin.lzr <soso.laz@gmail.com>
2024-04-02 19:10:46 +0200

    EX4: add dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,10 +55,12 @@ android {
 
 dependencies {
     val nav_version = "2.7.7"
+    val lifecycle_version = "2.7.0"
 
 
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycle_version")
     implementation("androidx.activity:activity-compose:1.7.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("io.coil-kt:coil-compose:2.6.0")
