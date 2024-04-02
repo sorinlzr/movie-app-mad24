864c31e876cca17f52d614f0f6de152c1e77f6ce
sorin.lzr <soso.laz@gmail.com>
2024-04-02 20:20:25 +0200

    EX4: remove unused import

--- app/src/main/java/com/example/movieappmad24/navigation/Navigation.kt
+++ app/src/main/java/com/example/movieappmad24/navigation/Navigation.kt
@@ -1,14 +1,12 @@
 package com.example.movieappmad24.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.movieappmad24.models.MovieViewModel
-import com.example.movieappmad24.models.getMovies
 import com.example.movieappmad24.screens.DetailScreen
 import com.example.movieappmad24.screens.HomeScreen
 import com.example.movieappmad24.screens.Screen
