bc3fddaf11d72ff51eeaead30ff6f094ba1713fb
sorin.lzr <soso.laz@gmail.com>
2024-05-10 19:32:51 +0200

    EX5: seed DB with sample data at startup

--- app/src/main/java/com/example/movieappmad24/data/MovieDatabase.kt
+++ app/src/main/java/com/example/movieappmad24/data/MovieDatabase.kt
@@ -4,8 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.movieappmad24.models.Movie
 import com.example.movieappmad24.models.MovieImage
+import com.example.movieappmad24.utils.getSampleMoviesWithImages
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Database(
     entities = [Movie::class, MovieImage::class],
@@ -25,6 +30,26 @@ abstract class MovieDatabase : RoomDatabase() {
             return instance ?: synchronized(this) {
                 Room.databaseBuilder(context, MovieDatabase::class.java, "movie_db")
                     .fallbackToDestructiveMigration()
+                    .addCallback(object : Callback() {
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            super.onCreate(db)
+                            val scope = CoroutineScope(Dispatchers.IO)
+                            scope.launch {
+                                val movieDao = getDatabase(context).movieDao()
+                                val movieImageDao = getDatabase(context).movieImageDao()
+
+                                val sampleMovies = getSampleMoviesWithImages()
+
+                                for (sampleMovie in sampleMovies) {
+                                    movieDao.add(sampleMovie.movie)
+
+                                    for (image in sampleMovie.images) {
+                                        movieImageDao.add(image)
+                                    }
+                                }
+                            }
+                        }
+                    })
                     .build()
                     .also {
                         instance = it
